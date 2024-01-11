fac87360d9ad49f617c6ed0a2b253bfc3d945129
adamishakad <adamishag1@gmail.com>
2024-01-11 10:38:08 -0800

    Updated

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@
         {
             Console.WriteLine("Hello, Professor!");
             Console.WriteLine("Hello class it is very great to meet you all");
-            Console.Write("Edsger W. Dijkstra");
+            Console.Write("1.Edsger W. Dijkstra");
             Console.WriteLine("Computer Science is no more about computers than astronomy is about telescopes");
-            Console.Write("Alan Kay");
+            Console.Write("2.Alan Kay");
             Console.WriteLine("The best way to predict the future is to invent it");
-            Console.Write("Grace Hopper");
+            Console.Write("3.Grace Hopper");
             Console.WriteLine("The most damaging phrase in the language is, we'are always done it this way");
-            Console.Write("Terry Pratchett");
+            Console.Write("4.Terry Pratchett");
             Console.WriteLine("It is not worth doing something unless someone, somewhere, would much rather you weren't doing it");
 
 
