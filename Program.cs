51e3b1b5c70fe2e867626e3d34be9916a995df04
adamishakad <adamishag1@gmail.com>
2024-01-15 20:15:27 -0800

    Updated Prograam File

--- Program.cs
+++ Program.cs
@@ -5,17 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Professor!");
-            Console.WriteLine("Hello class it is very great to meet you all");
-            Console.Write("1.Edsger W. Dijkstra");
-            Console.WriteLine("Computer Science is no more about computers than astronomy is about telescopes");
-            Console.Write("2.Alan Kay");
-            Console.WriteLine("The best way to predict the future is to invent it");
-            Console.Write("3.Grace Hopper");
-            Console.WriteLine("The most damaging phrase in the language is, we'are always done it this way");
-            Console.Write("4.Terry Pratchett");
-            Console.WriteLine("It is not worth doing something unless someone, somewhere, would much rather you weren't doing it");
-            Console.Write("1.Edsger W. Dijkstra");
-            Console.WriteLine("Computer Science is no more about computers than astronomy is about telescopes");        
+            Console.WriteLine(" Hello class it is very great to meet you all");
+            Console.WriteLine();
+            Console.Write("Edsger W. Dijkstra:");
+            Console.WriteLine(" Computer Science is no more about computers than astronomy is about telescopes");
+            Console.Write("Alan Kay:");
+            Console.WriteLine(" The best way to predict the future is to invent it");
+            Console.Write("Grace Hopper:");
+            Console.WriteLine(" The most damaging phrase in the language is, we'are always done it this way");
+            Console.Write("Terry Pratchett:");
+            Console.WriteLine(" It is not worth doing something unless someone, somewhere, would much rather you weren't doing it");
+            
 
 
         }
